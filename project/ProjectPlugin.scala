155ff5323f4642640a9bf162a847d0ab80e930da
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-03-02 09:54:50 +0100

    Update pureconfig, pureconfig-cats-effect to 0.12.3 (#69)
    * Update pureconfig, pureconfig-cats-effect to 0.12.3
    
    * fixup
    
    Co-authored-by: Ben Fradet <benjamin.fradet@gmail.com>

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -107,7 +107,7 @@ object ProjectPlugin extends AutoPlugin {
       val scala           = "2.12.10"
       val specs2          = "4.9.2"
       val avroHugger      = "1.0.0-RC22"
-      val pureconfig      = "0.12.2"
+      val pureconfig      = "0.12.3"
       val skeuomorph      = "0.0.22"
       val droste          = "0.8.0"
       val scalameta       = "4.3.0"
