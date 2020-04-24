04472352a22c294bb36bea9cc1f002f71eb3b487
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-04-25 00:43:28 +0200

    Update scalameta to 4.3.10 (#91)

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -105,7 +105,7 @@ object ProjectPlugin extends AutoPlugin {
       val pureconfig      = "0.12.3"
       val skeuomorph      = "0.0.22"
       val droste          = "0.8.0"
-      val scalameta       = "4.3.9"
+      val scalameta       = "4.3.10"
     }
 
     val clientSettings: Seq[Def.Setting[_]] = Seq(
